75c3dcd250766f0e3ef9cc184682d1c48f213748
menguzat <113802038+menguzat@users.noreply.github.com>
2023-05-02 15:12:50 +0300

    photonfix

--- classes/PhotonManager.js
+++ classes/PhotonManager.js
@@ -25,12 +25,15 @@ export class PhotonManager {
             this.photon.joinRoom(roomName);
         } else {
             console.log("room not found,creating");
-            this.photon.createRoom(roomName, { maxPlayers: 10,   broadcastPropsChangeToAll: true
+            this.photon.createRoom(roomName, {
+                maxPlayers: 10, broadcastPropsChangeToAll: true
             });
         }
     }
-    sendPlayerPositionUpdate(id, position,rotation) {
-        this.photon.myRoom().setCustomProperties({ ["pos-"+id.toString()]: position , ["rot-"+id.toString()]: rotation }, { webForward: true });
+
+    sendPlayerPositionUpdate(id, position, rotation) {
+        this.photon.raiseEvent(1, { id: id, position: position, rotation: rotation }, { receivers: Photon.LoadBalancing.Constants.ReceiverGroup.Others });
+        wwwwwwthis.photon.myRoom().setCustomProperties({ ["pos-" + id.toString()]: position, ["rot-" + id.toString()]: rotation }, { webForward: true });
     }
     setOnPlayerPositionUpdate(callback) {
         this.onPlayerPositionUpdate = callback;
@@ -39,12 +42,34 @@ export class PhotonManager {
         this.onJoinedRoom = callback;
     }
     onActorJoin(actor) {
-        this.photon.myRoom().setCustomProperty(actor.actorNr, { position: { x: 0, y: 0, z: 0 },rotation: { w:0, x: 0, y: 0, z: 0 }  });
+        // If the joining actor is the current client, no need to set its custom properties
+        if (this.photon.myActor().actorNr === actor.actorNr) {
+            return;
+        }
+
+        const position = { x: 0, y: 0, z: 0 };
+        const rotation = { w: 0, x: 0, y: 0, z: 0 };
+
+        // Check if there is an existing player with the same actor number
+        const existingPlayer = this.playerPositions.get(actor.actorNr.toString());
+        if (existingPlayer) {
+            position.x = existingPlayer.x;
+            position.y = existingPlayer.y;
+            position.z = existingPlayer.z;
+            rotation.w = existingPlayer.w;
+            rotation.x = existingPlayer.x;
+            rotation.y = existingPlayer.y;
+            rotation.z = existingPlayer.z;
+        }
+
+        // Set the custom properties for the joining actor
+        this.photon.myRoom().setCustomProperties({ [`pos-${actor.actorNr}`]: position, [`rot-${actor.actorNr}`]: rotation }, { webForward: true });
 
         if (this.actorJoinCallback) {
             this.actorJoinCallback(actor);
         }
     }
+s
     onActorLeave(actor) {
         if (this.actorLeaveCallback) {
             this.actorLeaveCallback(actor);
@@ -59,12 +84,15 @@ export class PhotonManager {
     onEvent(code, data) {
         // Handle Photon events here
         if (code === 0) {
-            const { id, actions,position, rotation } = data;
+            const { id, actions, position, rotation } = data;
             this.onPlayerPositionUpdate(id, actions, position, rotation);
         }
         else if (code === 1) { // Add this
-            const { id, position } = data;
+            
+            const { id, position, rotation  } = data;
             this.playerPositions.set(id, position, rotation);
+            this.onPlayerPositionUpdate(id,  position, rotation);
+
         }
     }
     onError(errorCode, errorMsg) {
