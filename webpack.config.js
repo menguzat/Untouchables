89366084626d9c34196134f0ee3f79adea1125e7
Montery59 <aniltrk132@gmail.com>
2023-05-04 14:27:44 +0300

    Webpack auto client-dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const WatchExternalFilesPlugin = require('webpack-watch-files-plugin').default
 
 module.exports = {
     mode: 'development',
@@ -9,6 +10,16 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist/client'),
     },
+    plugins:
+    [
+        new WatchExternalFilesPlugin({
+            files: [
+                path.resolve(__dirname, '../src/scripts/classes/Player.js'),
+                path.resolve(__dirname, '../src/scripts/classes/PhotonManager.js'),
+                path.resolve(__dirname, '../src/scripts/scenes/game2.js'),
+            ]
+        }),
+    ],
     resolve: {
         fallback: {
           crypto: require.resolve("crypto-browserify"),
